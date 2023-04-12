bca727c709e53acff476e296d2d3004f8dd7ca51
Toothless <81353028+Toothless5143@users.noreply.github.com>
2023-04-13 01:04:15 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@
 function subenum(){
 	subfinder -d $1 -all | tee $1.txt # using subfinder to get subdomains.
 	assetfinder --subs-only $1 | tee -a $1.txt # using assetfinder to get subdomains and appending the previous file.
-	amass enum -d $1  | tee -a $1.amass  # using amass tool to get subdomains in a different file.
-	python ~/Tools/ctfr/ctfr.py -d $1 | tee -a $1.ctrf # using ctfr to get subdomains and saving them in a different file.
+	amass enum -d $1  | tee -a $1.txt  # using amass tool to get subdomains and appending the previous file.
+	python ~/Tools/ctfr/ctfr.py -d $1 | tee -a $1.txt # using ctfr tool to get subdomains and appending the previous file.
 	cat  * | sort -u | uniq  | tee unique.txt # sorting 3 of the files and creating a new file only containing unique subdomains.
 	cat $1_uniq | dnsgen - | massdns -r /usr/share/wordlists/resolvers.txt -t A -o S -w massdns.txt # finding dns resolved ip of every single subdomains we found so far.
 	rm -rf $1.txt $1.amass $1.ctrf # removing the files we dont need for now, cause all of our scrapped subdomains are inside unique.txt and massdns.txt(resolved).

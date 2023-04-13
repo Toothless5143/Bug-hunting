05ec0562c34cc7d75d79a0255a59969749e4bbf1
Toothless <81353028+Toothless5143@users.noreply.github.com>
2023-04-13 22:47:03 +0600

    Update cloud_shell.sh

--- cloud_shell.sh
+++ cloud_shell.sh
@@ -8,23 +8,18 @@ subfinder -d $domain -all | tee subfinder.txt
 assetfinder --subs-only $domain | tee assetfinder.txt
 python ~/Tools/ctfr/ctfr.py -d $domain -o ctfr.txt
 
+ffuf -w /usr/share/wordlists/SecLists-master/Discovery/DNS/dns-Jhaddix.txt -u http://FUZZ.$domain -o fuzzing.txt
+cat ffuf.txt | jq -r '.results[].url' | sed 's/.*\///' | tee ffuf.txt
+
 # Run amass command and terminate after 30 minutes
 ( amass enum -d $domain | tee amass.txt ) & sleep 1800 && kill $(pgrep -f "amass enum -d $domain")
 wait
-
 cat * | sort -u | uniq | tee $domain.txt
 
 # Data processing
 cat $domain.txt | httpx -silent -fc 404 | awk -F/ '{print $3}' | tee $domain_live.txt
-
 cat $domain_live.txt | httpx -silent | subjs | tee $domain_subjs.txt
-
 cat $domain_live.txt | waybackurls | tee $domain_waybackurls.txt
 cat $domain_waybackurls.txt | grep "\.js" | tee $domain_waybackurls_js.txt
 cat $domain_waybackurls_js.txt | httpx -silent -fc 404 | tee $domain_waybackurls_live_js.txt
-
-# Removing useless files
-rm -rf $domain_waybackurls.txt $domain_waybackurls_js.txt
-
-
-
+rm -rf $domain_waybackurls_js.txt

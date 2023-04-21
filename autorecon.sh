cfafaa7fc81f21e27f59c1266f80c05f141a6a32
Toothless <81353028+Toothless5143@users.noreply.github.com>
2023-04-21 23:06:32 +0600

    Update autorecon.sh

--- autorecon.sh
+++ autorecon.sh
@@ -41,5 +41,8 @@ rm -rf waybackurls_dead.txt
 cat subdomains_live.txt | httpx -silent | subjs | tee subjs.txt
 cat waybackurls.txt | grep "\.js" | tee waybackurls_js.txt
 
+# Getting screenshots
+eyewitness -f subdomains_live.txt --web
+
 # Run paramspider for each domain in subdomains_live.txt
 cat subdomains_live.txt | xargs -I@ sh -c 'python ~/Tools/ParamSpider/paramspider.py -d @'
